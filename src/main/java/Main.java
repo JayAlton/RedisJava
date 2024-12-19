320439b28bd9577ae0a2220947e07a23b6a38e09
JayAlton <32560528+JayAlton@users.noreply.github.com>
2024-12-19 00:37:16 -0800

    Added loop for ping requests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,11 +17,9 @@ public class Main {
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
-      OutputStream outputStream = clientSocket.getOutputStream();
-      while(clientSocket.isConnected()) {
-        
-        outputStream.write("+PONG\r\n".getBytes());
-      }
+      
+      pingPong(clientSocket);
+      
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
@@ -33,6 +30,24 @@ public class Main {
       } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
       }
+    }
+  }
+
+  private static void pingPong(Socket clientSocket) {
+    try(BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));) {
+      String content;
+      while((content = reader.readLine()) != null) {
+        System.out.println("::" + content);
+        if("ping".equalsIgnoreCase(content)) {
+          writer.write("+PONG\r\n");
+          writer.flush();
+        } else if ("eof".equalsIgnoreCase(content)) {
+          System.out.println("eof");
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
 }
